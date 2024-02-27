db577be639b2eb05d314aef86405d5f7f6e0cb64
Rik Voorhaar <rik.voorhaar@gmail.com>
2024-02-27 22:16:50 +0100

    start work on compiler

---
+++ ijzer/examples/compiler_example.rs
@@ -0,0 +1,44 @@
+use anyhow::Result;
+use ijzer::{
+    compiler::CompilerContext,
+    node::{parse_line, ASTContext, Node},
+    tokens::lexer,
+};
+
+fn main() -> Result<()> {
+    let input: &str = "
+    x = 1
+    y = (+ (x 1))
+    fn z = * y I
+    * (z 1 2)
+    x y = (1 2)
+    ";
+    let mut symbol_table = ASTContext::new();
+    let mut parsed_lines: Vec<Node> = Vec::new();
+
+    for line in input.lines() {
+        let tokens = lexer(line)?;
+        if tokens.is_empty() {
+            continue;
+        }
+        println!("\ninput: {:?}", line);
+        let root = parse_line(tokens, &mut symbol_table)?;
+        println!("{:?}", root);
+        parsed_lines.push(root);
+    }
+    println!("-----------------------");
+
+    let root = parsed_lines.first().unwrap();
+    let mut compiler = CompilerContext::new(root.clone());
+    println!("{:?}", compiler.node_map.keys().collect::<Vec<_>>());
+    while let Some(node_id) = compiler.pop_next_parseable() {
+        println!("-> {:?}", compiler.node_map[&node_id]);
+        let stream = compiler.compile_node(node_id)?;
+        println!("{:?}", stream.to_string());
+
+        compiler.submit_as_parsed(node_id)
+
+    }
+
+    Ok(())
+}
--- ijzer/src/compiler.rs
+++ ijzer/src/compiler.rs
@@ -1,16 +1,21 @@
+use anyhow::Result;
 use std::collections::{HashMap, HashSet};
+use syn::Ident;
 
-use crate::node::Node;
+use proc_macro2::{Span, TokenStream};
+use quote::{quote, IdentFragment};
 
-struct CompilerContext {
-    node_map: HashMap<usize, Node>,
+use crate::{node::Node, operations::Operation, tokens::Token};
+
+pub struct CompilerContext {
+    pub node_map: HashMap<usize, Node>,
     parseable: Vec<usize>,
     parsed: HashSet<usize>,
-    parent: HashMap<usize, usize>,
+    pub parent: HashMap<usize, usize>,
 }
 
 impl CompilerContext {
-    fn new(root: Node) -> Self {
+    pub fn new(root: Node) -> Self {
         let mut node_map = HashMap::new();
         let mut leaves = vec![];
         let mut parent = HashMap::new();
@@ -36,21 +41,70 @@ impl CompilerContext {
         }
     }
 
-    fn submit_as_parsed(&mut self, node_id: usize) {
+    pub fn submit_as_parsed(&mut self, node_id: usize) {
         self.parsed.insert(node_id);
-        let parent = *self.parent.get(&node_id).unwrap();
-        let siblings = self
-            .node_map
-            .get(&parent)
-            .unwrap()
-            .operands
-            .iter()
-            .map(|n| n.id)
-            .collect::<Vec<_>>();
-        if siblings.iter().all(|s| self.parsed.contains(s)) {
-            self.parseable.push(parent);
+
+        if let Some(&parent) = self.parent.get(&node_id) {
+            let siblings = self
+                .node_map
+                .get(&parent)
+                .unwrap()
+                .operands
+                .iter()
+                .map(|n| n.id)
+                .collect::<Vec<_>>();
+            if siblings.iter().all(|s| self.parsed.contains(s)) {
+                self.parseable.push(parent);
+            }
+        }
+    }
+
+    pub fn pop_next_parseable(&mut self) -> Option<usize> {
+        self.parseable.pop()
+    }
+
+    pub fn compile_node(&mut self, node_id: usize) -> Result<TokenStream> {
+        let node = self.node_map.get(&node_id).unwrap();
+
+        let stream = match node.op {
+            Operation::Number(_) => Number::compile(node, self)?,
+            _ => NotImplemented::compile(node, self)?,
+        };
+
+        Ok(stream)
+    }
+
+    pub fn get_varname(&self, id: usize) -> Ident {
+        Ident::new(&format!("_{}", id), Span::call_site())
+    }
+}
+
+pub trait CompileNode {
+    fn compile(node: &Node, compiler: &CompilerContext) -> Result<TokenStream>;
+}
+
+struct Number;
+impl CompileNode for Number {
+    fn compile(node: &Node, compiler: &CompilerContext) -> Result<TokenStream> {
+        if let Operation::Number(number) = &node.op {
+            let varname = compiler.get_varname(node.id);
+            let val = number.value;
+
+            let res = quote! {
+                let #varname = #val;
+            };
+            Ok(res)
+        } else {
+            panic!("Expected number node, found {:?}", node);
         }
     }
 }
 
-// pub fn parse_node()
+struct NotImplemented;
+impl CompileNode for NotImplemented {
+    fn compile(_node: &Node, _compiler: &CompilerContext) -> Result<TokenStream> {
+        Ok(quote! {
+        panic!("Operation not implemented");
+        })
+    }
+}
