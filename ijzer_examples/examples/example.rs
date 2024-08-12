b9de940fc1c51c949a8a0674d2db939b81fa85b5
Rik Voorhaar <rik.voorhaar@gmail.com>
2024-08-12 14:01:21 +0200

    update macro to accept argument

--- ijzer_examples/examples/example.rs
+++ ijzer_examples/examples/example.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use ijzer::tensor::Tensor;
 use ijzer_macro::ijzer;
 
-#[ijzer]
+#[ijzer(f64)]
 fn test_function(x: Tensor<f64>) -> Tensor<f64> {
     r#"
     var x: T
@@ -10,7 +10,7 @@ fn test_function(x: Tensor<f64>) -> Tensor<f64> {
     z = - x y
     z = * y I
     u = [1.0,2.0,3.0]
-    v = @(-,+) [1] [2]
+    v = @(-,+) [1.0] [2.0]
     (* z [1.0] [-2.0])
     "#
 }
@@ -22,23 +22,4 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn test_function2(x: Tensor<f64>) -> Tensor<f64> {
-    let y = x
-        .apply_binary_op(&ijzer::tensor::Tensor::from_vec(vec![1.0], None), |a, b| {
-            a + b
-        })
-        .unwrap();
-    let z = x.apply_binary_op(&y, |a, b| a - b).unwrap();
-    let z = { |_12| y.apply_binary_op(&_12, |a, b| a * b).unwrap() };
-    let u = ijzer::tensor::Tensor::from_vec(vec![1.0, 2.0, 3.0], None);
-    let v = (|x1, x2| (|a| -a)((|a, b| a + b)(x1, x2)))(
-        ijzer::tensor::Tensor::from_vec(vec![1], None),
-        ijzer::tensor::Tensor::from_vec(vec![2], None),
-    );
-    z(ijzer::tensor::Tensor::from_vec(vec![1.0], None))
-        .apply_binary_op(
-            &ijzer::tensor::Tensor::from_vec(vec![-2.0], None),
-            |a, b| a * b,
-        )
-        .unwrap()
-}
+
