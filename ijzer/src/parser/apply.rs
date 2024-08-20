39ef61fccdfd046a67d19ba533c430b77e17c0ba
Rik Voorhaar <rik.voorhaar@gmail.com>
2024-08-20 11:00:12 +0200

    apply parser support

--- ijzer/src/parser/apply.rs
+++ ijzer/src/parser/apply.rs
@@ -1,4 +1,4 @@
-use super::{gather_operands, next_node_functional, Operation, ParseNode};
+use super::{gather_operands, next_node, Operation, ParseNode};
 use crate::ast_node::{ASTContext, Node, TokenSlice};
 use crate::syntax_error::SyntaxError;
 use crate::tokens::Token;
@@ -15,46 +15,23 @@ impl ParseNode for Apply {
         slice: TokenSlice,
         context: &mut ASTContext,
     ) -> Result<(Rc<Node>, TokenSlice)> {
-        let (function_nodes, rest) = next_node_functional(slice, context, None)?;
-        let wanted_types: Vec<Vec<IJType>> = function_nodes
-            .iter()
-            .map(|n| n.output_type.extract_signature().unwrap().input)
-            .collect();
-        let (operands, rest) = gather_operands(wanted_types, rest, context)?;
+        let (function_node, rest) = next_node(slice, context)?;
+        let signature = function_node.output_type.extract_signature().ok_or(
+            context.add_context_to_syntax_error(
+                SyntaxError::ExpectedFunction(function_node.to_string()).into(),
+                slice,
+            ),
+        )?;
+        let (operands, rest) = gather_operands(vec![signature.input.clone()], rest, context)?;
         let operands_types = operands
             .iter()
             .map(|n| n.output_type.clone())
             .collect::<Vec<IJType>>();
-        let function_node = function_nodes
-            .iter()
-            .find(|n| n.output_type.extract_signature().unwrap().input == operands_types)
-            .ok_or(
-                context.add_context_to_syntax_error(
-                    SyntaxError::FunctionVariantMismatch(
-                        function_nodes
-                            .iter()
-                            .map(|n| n.to_string())
-                            .collect::<Vec<String>>()
-                            .join(", "),
-                        operands_types
-                            .iter()
-                            .map(|t| t.to_string())
-                            .collect::<Vec<String>>()
-                            .join(", "),
-                    )
-                    .into(),
-                    slice,
-                ),
-            )?;
 
-        let function_signature = function_node.output_type.extract_signature().unwrap();
         let node_input_types = [vec![function_node.output_type.clone()], operands_types].concat();
-        let output_type = function_signature
-            .output
-            .first()
-            .ok_or(SyntaxError::InvalidType(
-                "Function has no output type".to_string(),
-            ))?;
+        let output_type = signature.output.first().ok_or(SyntaxError::InvalidType(
+            "Function has no output type".to_string(),
+        ))?;
         let operands = [vec![function_node.clone()], operands].concat();
 
         Ok((
@@ -75,14 +52,11 @@ mod tests {
     use super::*;
     use crate::ast_node::ASTContext;
     use crate::parser::test_utils::{parse_str, parse_str_no_context};
-    use crate::tokens::lexer;
-    use crate::types::FunctionSignature;
-
     use anyhow::Result;
 
     #[test]
     fn test_apply() -> Result<()> {
-        let (node, _) = parse_str_no_context(".+ 1 2")?;
+        let (node, _) = parse_str_no_context(".~+:Fn(S,S->S) 1 2")?;
         assert_eq!(node.op, Operation::Apply);
         assert_eq!(node.operands.len(), 3);
         Ok(())
@@ -93,7 +67,7 @@ mod tests {
         let mut context = ASTContext::new();
         parse_str("var f: Fn(S,S->S)", &mut context)?;
 
-        let node = parse_str(".f 1 2", &mut context)?;
+        let node = parse_str(".~f 1 2", &mut context)?;
         assert_eq!(node.op, Operation::Apply);
         assert_eq!(node.operands.len(), 3);
         Ok(())
@@ -104,7 +78,7 @@ mod tests {
         let mut context = ASTContext::new();
         parse_str("var f: Fn(S,S->S)", &mut context)?;
 
-        let node = parse_str(".@(-,f) 1 2", &mut context)?;
+        let node = parse_str(".~@(-,f) 1 2", &mut context)?;
         assert_eq!(node.op, Operation::Apply);
         assert_eq!(node.operands.len(), 3);
         Ok(())
@@ -115,7 +89,7 @@ mod tests {
         let mut context = ASTContext::new();
         parse_str("var f: Fn(S->Fn(S->S))", &mut context)?;
 
-        let node = parse_str("(f 1) 2", &mut context)?;
+        let node = parse_str(".(f 1) 2", &mut context)?;
         assert_eq!(node.op, Operation::Apply);
         assert_eq!(node.operands.len(), 2);
         Ok(())
