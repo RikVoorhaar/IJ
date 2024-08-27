7716cc35818ed53cff71d24a5700d58eeaf7e260
Rik Voorhaar <rik.voorhaar@gmail.com>
2024-08-27 09:06:05 +0200

    tests for number type generalized contraction

--- ijzer/src/parser/generalized_contraction.rs
+++ ijzer/src/parser/generalized_contraction.rs
@@ -27,6 +27,40 @@ impl GeneralizedContraction {
         let (g_node, rest) = next_node_specific_function(rest, context, Self::second_signature())?;
         Ok((f_node, g_node, rest))
     }
+    fn get_number_type_input_output(
+        f_node: &Rc<Node>,
+        g_node: &Rc<Node>,
+    ) -> Result<(Option<String>, Option<String>)> {
+        let f_number_type_output = f_node
+            .output_type
+            .extract_signature()
+            .unwrap()
+            .output
+            .extract_number_type()
+            .unwrap_or_default();
+        let f_number_type_input = f_node.output_type.extract_signature().unwrap().input[0]
+            .extract_number_type()
+            .unwrap_or_default();
+        let g_number_type = g_node
+            .output_type
+            .extract_signature()
+            .unwrap()
+            .output
+            .extract_number_type()
+            .unwrap_or_default();
+        if let (Some(n), Some(m)) = (f_number_type_input.clone(), g_number_type.clone()) {
+            if n != m {
+                return Err(SyntaxError::NumberTypeMismatch(n, m).into());
+            }
+        }
+        match (g_number_type, f_number_type_input, f_number_type_output) {
+            (Some(n1), _, Some(n2)) => Ok((Some(n1.clone()), Some(n2.clone()))),
+            (Some(n1), _, None) => Ok((Some(n1.clone()), Some(n1.clone()))),
+            (None, Some(n1), Some(n2)) => Ok((Some(n1.clone()), Some(n2.clone()))),
+            (None, None, Some(n2)) => Ok((None, Some(n2.clone()))),
+            _ => Ok((None, None)),
+        }
+    }
 }
 impl ParseNode for GeneralizedContraction {
     fn next_node(
@@ -35,11 +69,17 @@ impl ParseNode for GeneralizedContraction {
         context: &mut ASTContext,
     ) -> Result<(Rc<Node>, TokenSlice)> {
         let (f_node, g_node, rest) = Self::get_functional_part(slice, context)?;
+        let (number_type_input, number_type_output) =
+            Self::get_number_type_input_output(&f_node, &g_node)?;
         let (operands, rest) = gather_operands(
-            vec![vec![IJType::Tensor(None), IJType::Tensor(None)]],
+            vec![vec![
+                IJType::Tensor(number_type_input.clone()),
+                IJType::Tensor(number_type_input.clone()),
+            ]],
             rest,
             context,
         )?;
+        let output_type = IJType::Tensor(number_type_output.clone());
         let node = Node::new(
             Operation::GeneralizedContraction,
             vec![
@@ -48,7 +88,7 @@ impl ParseNode for GeneralizedContraction {
                 operands[0].output_type.clone(),
                 operands[1].output_type.clone(),
             ],
-            IJType::Tensor(None),
+            output_type,
             vec![f_node, g_node, operands[0].clone(), operands[1].clone()],
             context,
         )?;
@@ -71,38 +111,8 @@ impl ParseNodeFunctional for GeneralizedContraction {
             .into());
         }
         let (f_node, g_node, rest) = Self::get_functional_part(slice, context)?;
-        let f_number_type_output = f_node
-            .output_type
-            .extract_signature()
-            .unwrap()
-            .output
-            .extract_number_type()
-            .unwrap_or_default();
-        let f_number_type_input = f_node.output_type.extract_signature().unwrap().input[0]
-            .extract_number_type()
-            .unwrap_or_default();
-        let g_number_type = g_node
-            .output_type
-            .extract_signature()
-            .unwrap()
-            .output
-            .extract_number_type()
-            .unwrap_or_default();
-        match (f_number_type_input, g_number_type.clone()) {
-            (Some(n), Some(m)) => {
-                if n != m {
-                    return Err(SyntaxError::NumberTypeMismatch(n, m).into());
-                }
-            }
-            _ => {}
-        }
-        let (number_type_input, number_type_output) = match (f_number_type_output, g_number_type) {
-            (Some(n1), Some(n2)) => (Some(n1.clone()), Some(n2.clone())),
-            (Some(n1), None) => (Some(n1.clone()), Some(n1.clone())),
-            (None, Some(n2)) => (Some(n2.clone()), Some(n2.clone())),
-            _ => (None, None),
-        };
-
+        let (number_type_input, number_type_output) =
+            Self::get_number_type_input_output(&f_node, &g_node)?;
         let node = Node::new(
             Operation::GeneralizedContraction,
             vec![f_node.output_type.clone(), g_node.output_type.clone()],
@@ -189,6 +199,31 @@ mod tests {
         let mut context = ASTContext::new();
         parse_str("var f: Fn(T->S)", &mut context)?;
         parse_str("var g: Fn(N<f64>,N<f64>->N<f64>)", &mut context)?;
+        let node = parse_str("? f g [1] [2]", &mut context)?;
+        assert_eq!(node.op, Operation::GeneralizedContraction);
+        assert_eq!(node.output_type, IJType::Tensor(Some("f64".to_string())));
+
+        let mut context = ASTContext::new();
+        parse_str("var f: Fn(T<a>->S<b>)", &mut context)?;
+        parse_str("var g: Fn(N<a>,N<a>->N<a>)", &mut context)?;
+        let node = parse_str("? f g [1]<a> [2]<a>", &mut context)?;
+        assert_eq!(node.op, Operation::GeneralizedContraction);
+        assert_eq!(node.output_type, IJType::Tensor(Some("b".to_string())));
+
+        let mut context = ASTContext::new();
+        parse_str("var f: Fn(T<a>->S<b>)", &mut context)?;
+        parse_str("var g: Fn(N,N->N)", &mut context)?;
+        let node = parse_str("? f g [1]<a> [2]<a>", &mut context)?;
+        assert_eq!(node.op, Operation::GeneralizedContraction);
+        assert_eq!(node.output_type, IJType::Tensor(Some("b".to_string())));
+        Ok(())
+    }
+
+    #[test]
+    fn test_generalized_contraction_functional_number_type() -> Result<()> {
+        let mut context = ASTContext::new();
+        parse_str("var f: Fn(T->S)", &mut context)?;
+        parse_str("var g: Fn(N<f64>,N<f64>->N<f64>)", &mut context)?;
         let node = parse_str("~? f g", &mut context)?;
         assert_eq!(node.op, Operation::GeneralizedContraction);
         assert_eq!(
@@ -199,6 +234,38 @@ mod tests {
                     IJType::Tensor(Some("f64".to_string()))
                 ],
                 IJType::Tensor(Some("f64".to_string()))
+            ))
+        );
+
+        let mut context = ASTContext::new();
+        parse_str("var f: Fn(T<f64>->S<f32>)", &mut context)?;
+        parse_str("var g: Fn(N<f64>,N<f64>->N<f64>)", &mut context)?;
+        let node = parse_str("~? f g", &mut context)?;
+        assert_eq!(node.op, Operation::GeneralizedContraction);
+        assert_eq!(
+            node.output_type,
+            IJType::Function(FunctionSignature::new(
+                vec![
+                    IJType::Tensor(Some("f64".to_string())),
+                    IJType::Tensor(Some("f64".to_string()))
+                ],
+                IJType::Tensor(Some("f32".to_string()))
+            ))
+        );
+
+        let mut context = ASTContext::new();
+        parse_str("var f: Fn(T<a>->S<b>)", &mut context)?;
+        parse_str("var g: Fn(N,N->N)", &mut context)?;
+        let node = parse_str("~? f g", &mut context)?;
+        assert_eq!(node.op, Operation::GeneralizedContraction);
+        assert_eq!(
+            node.output_type,
+            IJType::Function(FunctionSignature::new(
+                vec![
+                    IJType::Tensor(Some("a".to_string())),
+                    IJType::Tensor(Some("a".to_string()))
+                ],
+                IJType::Tensor(Some("b".to_string()))
             ))
         );
         Ok(())
